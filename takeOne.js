9d8a995c4f2e9ab9c715347200ff2ffd12bb7d1c
Saša Prtvar <prtvar11@hotmail.com>
2017-05-10 18:16:47 -0600

    pop

--- takeOne.js
+++ takeOne.js
@@ -1,5 +1,6 @@
 let bottlesong = function {
     for (i = 0; i < bottlesong.length; i++);
     bottlesong.push(i + ' bottles of beer on the wall');
+    bottlesong.pop(' take one down pass it around ' -i);
 };
 console.log(bottlesong); 
