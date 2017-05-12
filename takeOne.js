c4c879b16be19b79d626fb7d3942047eb4ad93a6
Saša Prtvar <prtvar11@hotmail.com>
2017-05-11 23:48:47 -0600

    tino update

--- takeOne.js
+++ takeOne.js
@@ -1,6 +1,7 @@
-let bottlesong = function {
+let bottlesong = function () {
     for (i = 0; i < bottlesong.length; i++);
     bottlesong.push(i + ' bottles of beer on the wall');
+    tino update 
     
 };
 console.log(bottlesong); 
